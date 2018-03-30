b5202f8d05955d0ef2a741c89b6b1b4a85c5d573
朱晓进 <qhj1989@qq.com>
2018-03-30 10:03:19 +0800

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,13 +8,16 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct() {
+        $this->middleware('guest',['only'=>['create']]);
+    }
+
     /**
      * @name:create 创建用户
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      * @author:Storm <qhj1989@qq.com>
      */
-    public function create()
-    {
+    public function create() {
         return view('sessions.create');
     }
 
@@ -24,23 +27,21 @@ class SessionsController extends Controller
      * @return \Illuminate\Http\RedirectResponse
      * @author:Storm <qhj1989@qq.com>
      */
-    public function store(UserStoreRequest $request)
-    {
+    public function store(UserStoreRequest $request) {
         $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials,$request->has('remember'))) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         } else {
             session()->flash('danger', '很抱歉。您的邮箱和密码不匹配！');
             return redirect()->back();
         }
     }
 
-    public function destroy()
-    {
+    public function destroy() {
 
         Auth::logout();
-        session()->flash('success','您已经成功退出！');
+        session()->flash('success', '您已经成功退出！');
         return redirect()->route('home');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index() {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     /**
      * @name:create
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      * @author:Storm <qhj1989@qq.com>
      */
-    public function create()
-    {
+    public function create() {
         return view('users.create');
     }
 
@@ -24,8 +39,7 @@ class UserController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      * @author:Storm <qhj1989@qq.com>
      */
-    public function show(User $user)
-    {
+    public function show(User $user) {
         return view('users.show', compact('user'));
     }
 
@@ -34,8 +48,7 @@ class UserController extends Controller
      * @param Request $request
      * @author:Storm <qhj1989@qq.com>
      */
-    public function store(UserLoginRequest $request)
-    {
+    public function store(UserLoginRequest $request) {
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -46,4 +59,40 @@ class UserController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
+    /**
+     * @name:edit
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author:Storm <qhj1989@qq.com>
+     */
+    public function edit(User $user) {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * @name:update
+     * @param User $user
+     * @param UserUpdateRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @author:Storm <qhj1989@qq.com>
+     */
+    public function update(User $user, UserUpdateRequest $request) {
+        $this->authorize('update', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success', "个人资料更新成功");
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
